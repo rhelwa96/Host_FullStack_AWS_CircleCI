f9232c04d60c30d8d8371ad9af4ffc939ef914f1
rhelwa96 <rhelwa96@gmail.com>
2022-11-25 14:23:41 +0200

    fix deploy over frontend

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
 aws s3 cp --recursive ./www s3://host-udagram/
-aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://host-udagramx``/
+aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://host-udagram/
