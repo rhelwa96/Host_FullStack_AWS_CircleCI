724f69963e6972a51d26f1bd9ca044970c68cbed
rhelwa96 <rhelwa96@gmail.com>
2022-11-25 15:08:29 +0200

    changel app/env name over client/server

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,6 +1,6 @@
-eb init udagram-api --platform node.js-14 --region us-east-1
-eb create --sample udagram-api-dev
+eb init udagram-api-14 --platform node.js-14 --region us-east-1
+eb create --sample udagram-api-dev-14
 
-eb use udagram-api-dev
+eb use udagram-api-dev-14
 eb setenv POSTGRES_HOST=@POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD DB_PORT=$DB_PORT PORT=$PORT AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET URL=$URL JWT_SECRET=$JWT_SECRET
-eb deploy udagram-api-dev
+eb deploy udagram-api-dev-14
